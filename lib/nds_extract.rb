680a63c47f34d8ba398e2353810216a9521cce17
concurrent-server-0630 <andycruz008@gmail.com>
2020-02-14 17:56:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,15 +5,17 @@ def directors_totals(nds)
   pp nds
   totals = {}
   row = 0
-  col = 0
+  
   while row < nds.size do
     name = nds[row][:name]
     totals[name] = 0
+    col = 0
     while col < nds[row][:movies].length do
       totals[name] += nds[row][:movies][col][:worldwide_gross]
       col += 1
 
  end 
+ row +=1
 end
 totals
 end
